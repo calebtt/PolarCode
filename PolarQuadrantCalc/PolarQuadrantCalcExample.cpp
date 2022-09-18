52aeff9c2ca3a74751b6108a18f31e73faec0858
calebtt <calebktaylor@gmail.com>
2022-09-18 00:06:36 -0500

    added some performance alternatives and tests.

--- PolarQuadrantCalc/PolarQuadrantCalcExample.cpp
+++ PolarQuadrantCalc/PolarQuadrantCalcExample.cpp
@@ -3,6 +3,27 @@
 #include "GetterExit.h"
 static constexpr auto newl = '\n';
 
+
+struct MouseSettings
+{
+	//PolarRadiusValueMax is the sentinel abs value of a computed polar radius.
+	static constexpr int PolarRadiusValueMax{ 39'000 };
+	//Microseconds Min is the minimum delay for the thumbstick axis thread loop at the highest thumbstick value.
+	static constexpr int MICROSECONDS_MIN{ 1100 };
+	//Microseconds Max is the maximum delay for the thumbstick axis thread loop at the lowest thumbstick value.
+	static constexpr int MICROSECONDS_MAX{ 24'000 };
+};
+MouseSettings m_mouse_settings{};
+
+[[nodiscard]] auto ConvertMagnitudesToDelays(const auto xPolarDelay, const auto yPolarDelay) -> std::pair<size_t, size_t>
+{
+	const float xPercentOfRadius = xPolarDelay / static_cast<float>(m_mouse_settings.PolarRadiusValueMax);
+	const float yPercentOfRadius = yPolarDelay / static_cast<float>(m_mouse_settings.PolarRadiusValueMax);
+	auto xResult = static_cast<size_t>(std::lerp(m_mouse_settings.MICROSECONDS_MIN, m_mouse_settings.MICROSECONDS_MAX, xPercentOfRadius));
+	auto yResult = static_cast<size_t>(std::lerp(m_mouse_settings.MICROSECONDS_MIN, m_mouse_settings.MICROSECONDS_MAX, yPercentOfRadius));
+	return { xResult, yResult };
+}
+
 void DoPolarCalc(const XINPUT_STATE &state)
 {
 	using std::cout;
@@ -16,12 +37,50 @@ void DoPolarCalc(const XINPUT_STATE &state)
 	const auto [polarRadius, polarTheta] = polarComplete.polar_info;
 	//alias for quadrant range pair and quadrant number
 	const auto [rangePair, quadResult] = polarComplete.quadrant_info;
-
+	//convert magnitudes to delays (directly)
+	const auto [xDelay, yDelay] = ConvertMagnitudesToDelays(xMagnitude, yMagnitude);
 	cout << "Radius: " << polarRadius << newl;
 	cout << "Theta Angle: " << polarTheta << " Half Angle is: " << ((rangePair.second - rangePair.first) / 2.0) + rangePair.first << newl;
 	cout << "X adjusted mag: " << xMagnitude << newl;
 	cout << "Y adjusted mag: " << yMagnitude << newl;
 	cout << "Quadrant: " << quadResult << " With bounds: " << std::get<0>(rangePair) << "," << std::get<1>(rangePair) << newl;
+	cout << "-------" << newl;
+	cout << "Xdelay:" << xDelay << newl;
+	cout << "Ydelay:" << yDelay << newl;
+	cout << "-------" << newl;
+	cout << "Cos x:" << std::cos(polarTheta) * polarRadius << newl;
+	cout << "Sin y:" << std::sin(polarTheta) * polarRadius << newl;
+	cout << "-------" << newl;
+}
+
+void DoScaleInputValues(const XINPUT_STATE& state)
+{
+	using std::cout;
+	auto LogFn = [](const char* str) { std::cerr << str << newl; };
+	sds::PolarCalc pc(32'766, LogFn);
+	//get info members
+	const auto polarComplete = pc.ComputePolarCompleteInfo(state.Gamepad.sThumbRX, state.Gamepad.sThumbRY);
+	//alias for adjusted magnitude values
+	const auto [xMagnitude, yMagnitude] = polarComplete.adjusted_magnitudes;
+	//alias for polar radius and polar theta angle
+	const auto [polarRadius, polarTheta] = polarComplete.polar_info;
+	//alias for quadrant range pair and quadrant number
+	const auto [rangePair, quadResult] = polarComplete.quadrant_info;
+	//convert magnitudes to delays (directly)
+	const auto [xDelay, yDelay] = ConvertMagnitudesToDelays(xMagnitude, yMagnitude);
+	cout << "Hardware: [X]:" << state.Gamepad.sThumbRX << " [Y]:" << state.Gamepad.sThumbRY << newl;
+	cout << "-------" << newl;
+	cout << "Radius: " << polarRadius << newl;
+	cout << "Theta Angle: " << polarTheta << " Half Angle is: " << ((rangePair.second - rangePair.first) / 2.0) + rangePair.first << newl;
+	cout << "X adjusted mag: " << xMagnitude << newl;
+	cout << "Y adjusted mag: " << yMagnitude << newl;
+	cout << "Quadrant: " << quadResult << " With bounds: " << std::get<0>(rangePair) << "," << std::get<1>(rangePair) << newl;
+	cout << "-------" << newl;
+	cout << "Xdelay:" << xDelay << newl;
+	cout << "Ydelay:" << yDelay << newl;
+	cout << "-------" << newl;
+	cout << "Cos x:" << std::cos(polarTheta) * polarRadius << newl;
+	cout << "Sin y:" << std::sin(polarTheta) * polarRadius << newl;
 	cout << "-------" << newl;
 }
 
@@ -40,7 +99,8 @@ int main(int argc, const char* argv[])
 		const auto res = XInputGetState(0, &state);
 		if (res == ERROR_SUCCESS)
 		{
-			DoPolarCalc(state);
+			//DoPolarCalc(state);
+			DoScaleInputValues(state);
 		}
 		std::this_thread::sleep_for(std::chrono::milliseconds(4));
 	}
--- PolarQuadrantCalcTest/PolarCalcTest.cpp
+++ PolarQuadrantCalcTest/PolarCalcTest.cpp
@@ -1,8 +1,10 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "../PolarQuadrantCalc/PolarCalc.h"
 #include <chrono>
 #include <format>
+#include "../PolarQuadrantCalc/PolarCalc.h"
+#include "../PolarQuadrantCalc/PolarCalcFaster.h"
+#include "../PolarQuadrantCalc/PolarTransform.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,6 +13,16 @@ namespace PolarQuadrantCalcTest
 	TEST_CLASS(PolarCalcTest)
 	{
 		static constexpr auto MAGNITUDE_MAX{ 28'000.0 };
+
+		void PrintDurationUnit(const auto t1, const auto t2) const noexcept
+		{
+			std::stringstream ss;
+			const std::chrono::duration<double, std::milli> fp_ms = t2 - t1;
+			ss << fp_ms;
+			//ss << std::chrono::duration_cast<ChronoType_t> (t2 - t1);
+			//const auto timeResult = std::chrono::duration_cast<ChronoType_t> (timeResultEnd - timeResultBegin);
+			Logger::WriteMessage(std::format("Total time at {0} ...\n", ss.str().c_str()).c_str());
+		}
 	public:
 		[[nodiscard]] static constexpr bool IsWithin(const auto result, const auto testVal, const auto within) noexcept
 		{
@@ -24,7 +36,7 @@ namespace PolarQuadrantCalcTest
 			using std::cout;
 			static constexpr char newl{ '\n' };
 			static constexpr auto WITHIN_VAL{ 2000 };
-			static constexpr size_t MAX_ITERATIONS{ 100'000 };
+			static constexpr size_t MAX_ITERATIONS{ 1'000'000 };
 			//theoretical hardware value max
 			constexpr auto SMax = std::numeric_limits<SHORT>::max();
 			//theoretical hardware value max
@@ -69,8 +81,127 @@ namespace PolarQuadrantCalcTest
 			const auto timeResultEnd = std::chrono::system_clock::now();
 			Logger::WriteMessage(std::format("Starting at {0} ...\n", timeResultBegin).c_str());
 			Logger::WriteMessage(std::format("Ending at {0} ...\n", timeResultEnd).c_str());
-			const auto timeResult = std::chrono::duration_cast<std::chrono::microseconds> (timeResultEnd - timeResultBegin);
-			Logger::WriteMessage(std::format("Total time at {0} ...\n", timeResult).c_str());
+			PrintDurationUnit(timeResultBegin, timeResultEnd);
+			//const auto timeResult = std::chrono::duration_cast<std::chrono::microseconds> (timeResultEnd - timeResultBegin);
+			//Logger::WriteMessage(std::format("Total time at {0} ...\n", timeResult).c_str());
+		}
+
+		TEST_METHOD(TestComputeMagnitudesTransform)
+		{
+			// Test fn for the transform variant of the polar calc
+			using std::wstring;
+			using std::wcout;
+			using std::cout;
+			static constexpr char newl{ '\n' };
+			static constexpr auto WITHIN_VAL{ 2000 };
+			static constexpr size_t MAX_ITERATIONS{ 1'000'000 };
+			static constexpr int MAGNITUDE_MAX_LOCAL{ 28'000 };
+			//theoretical hardware value max
+			constexpr auto SMax = std::numeric_limits<SHORT>::max();
+			//theoretical hardware value max
+			constexpr auto SMin = std::numeric_limits<SHORT>::min();
+			//error logging fn
+			auto LogFn = [this](const char* str) { Logger::WriteMessage(str); Assert::IsTrue(false); };
+			
+			//lambda for testing
+			auto ComputeAndShow = [&](const int x, const int y, const std::string& msg, const int xShouldBe, const int yShouldBe, const bool printMessages = true)
+			{
+				//polar transform instance
+				sds::PolarTransform pc(x, y, LogFn, static_cast<int>(MAGNITUDE_MAX_LOCAL));
+				const auto completeInfo = pc.get();
+				const auto [xMax, yMax] = completeInfo.adjusted_magnitudes;
+				std::stringstream ss;
+				ss << "Magnitude of x at " + msg + ": " << xMax << newl;
+				ss << "Magnitude of y at " + msg + ": " << yMax << newl;
+				ss << "-------" << newl;
+				if (printMessages)
+					Logger::WriteMessage(ss.str().c_str());
+				const auto xResult = IsWithin(xMax, xShouldBe, WITHIN_VAL);
+				const auto yResult = IsWithin(yMax, yShouldBe, WITHIN_VAL);
+				Assert::IsTrue(xResult, L"X axis not within acceptable parameters...");
+				Assert::IsTrue(yResult, L"Y axis not within acceptable parameters...");
+			};
+			ComputeAndShow(SMax, SMax, "short-max,short-max", MAGNITUDE_MAX_LOCAL, MAGNITUDE_MAX_LOCAL);
+			ComputeAndShow(SMin, SMax, "short-min,short-max", MAGNITUDE_MAX_LOCAL, MAGNITUDE_MAX_LOCAL);
+			ComputeAndShow(SMin, SMin, "short-min,short-min", MAGNITUDE_MAX_LOCAL, MAGNITUDE_MAX_LOCAL);
+			ComputeAndShow(SMin / 2, SMin / 2, "short-min/2,short-min/2", (SMax) / 2, (SMax) / 2);
+			//const auto completeInfo = sds::PolarTransform(SMax / 2, SMax / 2).get();
+			const auto [xMax, yMax] = sds::PolarTransform(SMax / 2, SMax / 2).get().adjusted_magnitudes;
+			Assert::AreEqual(static_cast<int>(xMax), static_cast<int>(yMax), L"");
+
+			//Running several iterations in a loop with begin and end time stamps.
+			const auto timeResultBegin = std::chrono::system_clock::now();
+			for (size_t i = 0; i < MAX_ITERATIONS; ++i)
+			{
+				ComputeAndShow(SMax, SMax, "short-max,short-max", MAGNITUDE_MAX_LOCAL, MAGNITUDE_MAX_LOCAL, false);
+				ComputeAndShow(SMin, SMax, "short-min,short-max", MAGNITUDE_MAX_LOCAL, MAGNITUDE_MAX_LOCAL, false);
+				ComputeAndShow(SMin, SMin, "short-min,short-min", MAGNITUDE_MAX_LOCAL, MAGNITUDE_MAX_LOCAL, false);
+				ComputeAndShow(SMin / 2, SMin / 2, "short-min/2,short-min/2", (SMax) / 2, (SMax) / 2, false);
+			}
+			const auto timeResultEnd = std::chrono::system_clock::now();
+			Logger::WriteMessage(std::format("Starting at {0} ...\n", timeResultBegin).c_str());
+			Logger::WriteMessage(std::format("Ending at {0} ...\n", timeResultEnd).c_str());
+			PrintDurationUnit(timeResultBegin, timeResultEnd);
+			//const auto timeResult = std::chrono::duration_cast<std::chrono::microseconds> (timeResultEnd - timeResultBegin);
+			//Logger::WriteMessage(std::format("Total time at {0} ...\n", timeResult).c_str());
+		}
+
+		TEST_METHOD(TestComputeMagnitudesFaster)
+		{
+			using std::wstring;
+			using std::wcout;
+			using std::cout;
+			static constexpr char newl{ '\n' };
+			static constexpr auto WITHIN_VAL{ 2000 };
+			static constexpr size_t MAX_ITERATIONS{ 1'000'000 };
+			static constexpr int MAGNITUDE_MAX_LOCAL{ 28'000 };
+			//theoretical hardware value max
+			constexpr auto SMax = std::numeric_limits<SHORT>::max();
+			//theoretical hardware value max
+			constexpr auto SMin = std::numeric_limits<SHORT>::min();
+			//error logging fn
+			auto LogFn = [this](const char* str) { Logger::WriteMessage(str); Assert::IsTrue(false); };
+			//polar calc instance
+			sds::PolarCalcFaster pc(MAGNITUDE_MAX_LOCAL, LogFn);
+			//lambda for testing
+			auto ComputeAndShow = [&](const double x, const double y, const std::string& msg, const double xShouldBe, const double yShouldBe, const bool printMessages = true)
+			{
+				const auto completeInfo = pc.ComputePolarCompleteInfo(x, y);
+				const auto [xMax, yMax] = completeInfo.adjusted_magnitudes;
+				std::stringstream ss;
+				ss << "Magnitude of x at " + msg + ": " << xMax << newl;
+				ss << "Magnitude of y at " + msg + ": " << yMax << newl;
+				ss << "-------" << newl;
+				if (printMessages)
+					Logger::WriteMessage(ss.str().c_str());
+				const auto xResult = IsWithin(xMax, xShouldBe, WITHIN_VAL);
+				const auto yResult = IsWithin(yMax, yShouldBe, WITHIN_VAL);
+				Assert::IsTrue(xResult, L"X axis not within acceptable parameters...");
+				Assert::IsTrue(yResult, L"Y axis not within acceptable parameters...");
+			};
+			ComputeAndShow(SMax, SMax, "short-max,short-max", MAGNITUDE_MAX_LOCAL, MAGNITUDE_MAX_LOCAL);
+			ComputeAndShow(SMin, SMax, "short-min,short-max", MAGNITUDE_MAX_LOCAL, MAGNITUDE_MAX_LOCAL);
+			ComputeAndShow(SMin, SMin, "short-min,short-min", MAGNITUDE_MAX_LOCAL, MAGNITUDE_MAX_LOCAL);
+			ComputeAndShow(SMin / 2.0, SMin / 2.0, "short-min/2,short-min/2", (SMax) / 2.0, (SMax) / 2.0);
+			const auto completeInfo = pc.ComputePolarCompleteInfo(SMax / 2.0, SMax / 2.0);
+			const auto [xMax, yMax] = completeInfo.adjusted_magnitudes;
+			Assert::AreEqual(static_cast<int>(xMax), static_cast<int>(yMax), L"");
+
+			//Running several iterations in a loop with begin and end time stamps.
+			const auto timeResultBegin = std::chrono::system_clock::now();
+			for (size_t i = 0; i < MAX_ITERATIONS; ++i)
+			{
+				ComputeAndShow(SMax, SMax, "short-max,short-max", MAGNITUDE_MAX_LOCAL, MAGNITUDE_MAX_LOCAL, false);
+				ComputeAndShow(SMin, SMax, "short-min,short-max", MAGNITUDE_MAX_LOCAL, MAGNITUDE_MAX_LOCAL, false);
+				ComputeAndShow(SMin, SMin, "short-min,short-min", MAGNITUDE_MAX_LOCAL, MAGNITUDE_MAX_LOCAL, false);
+				ComputeAndShow(SMin / 2.0, SMin / 2.0, "short-min/2,short-min/2", (SMax) / 2.0, (SMax) / 2.0, false);
+			}
+			const auto timeResultEnd = std::chrono::system_clock::now();
+			Logger::WriteMessage(std::format("Starting at {0} ...\n", timeResultBegin).c_str());
+			Logger::WriteMessage(std::format("Ending at {0} ...\n", timeResultEnd).c_str());
+			PrintDurationUnit(timeResultBegin, timeResultEnd);
+			//const auto timeResult = std::chrono::duration_cast<std::chrono::microseconds> (timeResultEnd - timeResultBegin);
+			//Logger::WriteMessage(std::format("Total time at {0} ...\n", timeResult).c_str());
 		}
 	};
 }
